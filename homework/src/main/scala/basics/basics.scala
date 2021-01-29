9f633a99605f59902d69196b1ad9498aa2eb22ee
Stanislau9 <70797922+Stanislau9@users.noreply.github.com>
2021-01-29 18:27:46 +0300

    Fix null exception

--- homework/src/main/scala/basics/basics.scala
+++ homework/src/main/scala/basics/basics.scala
@@ -1,14 +1,19 @@
 package basics
 
+import scala.annotation.tailrec
+import scala.math.abs
+
 object basics {
 
-  def gcd(a: Int, b: Int): Int = (math.abs(a), math.abs(b)) match {
-    case (0, _) => math.abs(b)
-    case (_, 0) => math.abs(a)
-    case (aa, ba) if a > b => gcd(ba, aa % ba)
-    case (aa, ba) => gcd(aa, ba % aa)
+  @tailrec
+  def gcd(a: Int, b: Int): Option[Int] = (abs(a), abs(b)) match {
+    case (0, 0) => None
+    case (0, _)             => Some(abs(b))
+    case (_, 0)             => Some(abs(a))
+    case (aa, ba) if a > b  => gcd(ba, aa % ba)
+    case (aa, ba)           => gcd(aa, ba % aa)
   }
 
-  def lcm(a: Int, b: Int): Int = math.abs(a * b) / gcd(a, b)
+  def lcm(a: Int, b: Int): Option[Int] = gcd(a, b).map(gcd => abs(a * b) / gcd)
 
 }
